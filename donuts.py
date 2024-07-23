b1c2fa4f6177593ee0bc772be8f8acaa20a971b0
Ken <ken.girard@gmail.com>
2024-07-22 21:59:23 -0500

    4: Added inpout, totally added in a free donut/dozen, changed printing

--- donuts.py
+++ donuts.py
@@ -1,14 +1,24 @@
 def donut_boxes():
-  boxes = range(3)
-  columns = range(4)
-  rows = range(3)
+  donuts = int(input("Enter the number of donuts: "))
   
-  for box in boxes: 
-      for column in columns:
-        y = ''
-        for row in rows: 
-           y = y + "0"
-        print(y) # prints the row
-      print("\n") # Creates a blank line between each box
+  # Calculate total donuts, including extra
+  total_donuts = donuts + donuts // 12
+  
+  box_number = 1
+  while total_donuts > 0:
+    print(f"Box {box_number}")
+    donuts_in_box = min(12, total_donuts)
+    total_donuts -= donuts_in_box
+        
+    for row in range(4): 
+      for column in range(3):
+        if donuts_in_box > 0:
+          print("O", end="")
+          donuts_in_box -= 1
+        else:
+          print(" ", end="")
+      print()
+    print()
+    box_number += 1
 	
 donut_boxes()
