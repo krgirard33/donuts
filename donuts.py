e3f3d31a37974396c616998f5d09190d8052ce5e
Ken <ken.girard@gmail.com>
2024-07-22 19:32:38 -0500

    Step 2

--- donuts.py
+++ donuts.py
@@ -1,7 +1,10 @@
 def donut_boxes():
   donuts = range(12)
+  boxes = range(3)
   
-  for donut in donuts:
-    print("O")
+  for box in boxes: 
+      for donut in donuts:
+        print("O")
+      print("\n") # Creates a blank line between each box
 	
 donut_boxes()
