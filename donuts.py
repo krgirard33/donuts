a90793c148e286a8fcbc4472da86d5d6ef6c26f1
Ken <ken.girard@gmail.com>
2024-07-22 19:53:35 -0500

    3: They look like boxes now

--- donuts.py
+++ donuts.py
@@ -1,10 +1,14 @@
 def donut_boxes():
-  donuts = range(12)
   boxes = range(3)
+  columns = range(4)
+  rows = range(3)
   
   for box in boxes: 
-      for donut in donuts:
-        print("O")
+      for column in columns:
+        y = ''
+        for row in rows: 
+           y = y + "0"
+        print(y) # prints the row
       print("\n") # Creates a blank line between each box
 	
 donut_boxes()
